94a709cc8a5b4021e8742b06edeae03729b385c9
Mostafa Fathy Ali Amer <96601097+tofyfathy12@users.noreply.github.com>
2025-04-13 16:55:17 +0200

    modified:   PolynomialSolver.java

--- PolynomialSolver.java
+++ PolynomialSolver.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class PolynomialSolver implements IPolynomialSolver {
     DLL[] polynomials = new DLL[4];
@@ -145,8 +146,114 @@ public class PolynomialSolver implements IPolynomialSolver {
     }
 
     public int[][] multiply(char poly1, char poly2) {
-        // TODO Auto-generated method stub
-        return null;
+        DLL p1 = polynomials[poly1 - 'A'];
+        DLL p2 = polynomials[poly2 - 'A'];
+        int diff = Math.abs(p1.size() - p2.size());
+        if (p1.size() < p2.size()) {
+            for (int i = 0; i < diff; i++) {
+                p1.add(0, 0);
+            }
+        }
+        else {
+            for (int i = 0; i < diff; i++) {
+                p2.add(0, 0);
+            }
+        }
+        
+        DLL result = polynomials[3]; // the variable R to store result
+        result.clear();
 
+        DLLNode i = p1.getHead();
+        DLLNode j = p2.getHead();
+        // i and j are nodes
+        int sum = 0;
+        DLLNode lastRef = i;
+        while (i != null) {
+            sum += (int)i.getElement() * (int)j.getElement();
+            if (i != p1.getHead()) {
+                i = i.getPrev();
+                j = j.getNext();
+            }
+            else {
+                result.add(sum);
+                sum = 0;
+                i = lastRef.getNext();
+                lastRef = lastRef.getNext();
+                j = p2.getHead();
+            }
+        }
+        i = p1.getTail();
+        j = p2.getHead().getNext();
+        lastRef = j;
+        while (j != null) {
+            sum += (int)i.getElement() + (int)j.getElement();
+            if (j != p2.getTail()) {
+                i = i.getPrev();
+                j = j.getNext();
+            }
+            else {
+                result.add(sum);
+                sum = 0;
+                j = lastRef.getNext();
+                lastRef = lastRef.getNext();
+                i = p2.getTail();
+            }
+        }
+        result.printList(); // debugging
+        while ((int)(result.getHead().getElement()) == 0)
+        {
+            result.remove(0);
+        }
+        result.printList(); // debugging
+
+        int length = result.size();
+        int[][] final_result = new int[length][2];
+        DLLNode curr = result.getHead();
+        for (int k = 0; k < length; k++) {
+            final_result[k][0] = length - k - 1;
+            final_result[k][1] = (int)curr.getElement();
+            curr = curr.getNext();
+        }
+        return final_result;
+    }
+
+    public static void main(String[] args) {
+        PolynomialSolver solver = new PolynomialSolver();
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        if (input.equals("set")) {
+            String characteString = scan.nextLine();
+            char poly = characteString.charAt(0);
+            input = scan.nextLine();
+            input = input.replaceAll("\\[|\\]", "");
+            String[] inputList = input.split(", *");
+            int length = inputList.length;
+            int[][] terms = new int[length][2];
+            for (int i = 0; i < length; i++) {
+                terms[i][0] = length - 1 - i;
+                terms[i][1] = Integer.parseInt(inputList[i]);
+            }
+            solver.setPolynomial(poly, terms);
+        }
+        else if (input.equals("print")) {
+            
+        }
+        else if (input.equals("add")) {
+            
+        }
+        else if (input.equals("sub")) {
+            
+        }
+        else if (input.equals("mult")) {
+            
+        }
+        else if (input.equals("clear")) {
+            
+        }
+        else if (input.equals("eval")) {
+            
+        }
+        
+        scan.close();
     }
 }
